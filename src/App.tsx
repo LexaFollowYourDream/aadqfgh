9b782cd0faf82834ae7f1cca1c9c0df2f57cdec0
LexaFollowYourDream <aleksejkundenko10@gmail.com>
2023-02-25 22:07:37 +0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import styles from './App.module.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [count, setCount] = useState(0)
+
+
+    const onClickHandler = () => {
+        if (count >= 5) {
+            return
+           // return alert('максимальное число')
+        }
+        setCount((prevState) => prevState + 1)
+    }
+
+    const onClickHandlerZero = () => {
+        setCount(0)
+    }
+
+
+    return (
+        <div className={styles.fon}>
+            <div className={styles.counter}>
+                <div className={styles.buttons_number}>
+                    <h1 className={count === 5 ? `${styles.number}` : `${styles.numberCounter}`}>{count}</h1>
+                    <div className={styles.buttons}>
+                        <button className={styles.button_incActive} onClick={onClickHandler}> inc</button>
+                        <button style={{background: count === 0 ? 'blue' : undefined}}
+                                className={styles.button_resetActive} onClick={onClickHandlerZero}> reset
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
+//style={{color: count === 5 ? 'red' : undefined}}
 export default App;
